ad5226c7d5238cc1788e0038f6145d259c894903
dengo25 <dengo25.dev@gmail.com>
2025-11-30 17:49:51 +0900

    회원 애플리케이션의 포트 정의

--- src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -23,11 +23,11 @@ class MemberTest {
         return encode(password).equals(passwordHash);
       }
     };
-    member = Member.create(new MemberCreateRequest("toby@splearn.app", "Toby", "secret"), passwordEncoder);
+    member = Member.register(new MemberRegisterRequest("toby@splearn.app", "Toby", "secret"), passwordEncoder);
   }
   
   @Test
-  void createMember() {
+  void registerMember() {
     assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
   }
   
@@ -109,10 +109,10 @@ class MemberTest {
   void invalidEmail() {
     
     assertThatThrownBy(() ->
-        Member.create(new MemberCreateRequest("invalid email", "Toby", "secret"), passwordEncoder))
+        Member.register(new MemberRegisterRequest("invalid email", "Toby", "secret"), passwordEncoder))
         .isInstanceOf(IllegalArgumentException.class);
     
-    Member.create(new MemberCreateRequest("toby@email.com", "Toby", "secret"), passwordEncoder);
+    Member.register(new MemberRegisterRequest("toby@email.com", "Toby", "secret"), passwordEncoder);
     
   }
   
