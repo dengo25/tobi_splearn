7c18b620be8a9374fc4080bbede3af95dfb5cd42
dengo25 <dengo25.dev@gmail.com>
2025-12-02 19:08:44 +0900

    표준 유효성 검사 도구를 이용한 요청 데이터 검증

--- src/test/java/tobyspring/splearn/application/provided/MemberRegisterTest.java
+++ src/test/java/tobyspring/splearn/application/provided/MemberRegisterTest.java
@@ -1,14 +1,12 @@
 package tobyspring.splearn.application.provided;
 
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 import tobyspring.splearn.SplearnTestConfiguration;
-import tobyspring.splearn.domain.DuplicateEmailException;
-import tobyspring.splearn.domain.Member;
-import tobyspring.splearn.domain.MemberFixture;
-import tobyspring.splearn.domain.MemberStatus;
+import tobyspring.splearn.domain.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -36,5 +34,19 @@ public record MemberRegisterTest(MemberRegister memberRegister) {
         .isInstanceOf(DuplicateEmailException.class);
     
   }
+  
+  @Test
+  void memberRegisterRequestFail() {
+    
+    extracted(new MemberRegisterRequest("toby@splearn.app", "Toby", "longsecret"));
+    extracted(new MemberRegisterRequest("toby@splearn.app", "Charlie_____________________________", "longsecret"));
+    extracted(new MemberRegisterRequest("tobysplearn.app", "Charlie", "longsecret"));
+  }
+  
+  private void extracted(MemberRegisterRequest invalid) {
+    assertThatThrownBy(() -> memberRegister.register(invalid))
+        .isInstanceOf(ConstraintViolationException.class);
+  }
+  
   
 }
